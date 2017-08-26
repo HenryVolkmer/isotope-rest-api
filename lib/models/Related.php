3821bfd1d774b0f005f344db2648a02e1d2d68eb
Henry Lamorski <henry.lamorski@mailbox.org>
2017-08-26 16:40:59 +0200

    Bugfix for Related

--- lib/models/Related.php
+++ lib/models/Related.php
@@ -9,11 +9,41 @@ class Related extends Generic
 	
 	public function rules()
 	{
-		array(
+		return array(
 			array('category','numerical','integerOnly'=>true),
-			array('products', 'safe'),
+			array('products', 'skuToIds'),
 		);
 	}
+	
+	/** SKU => id **/
+	public function skuToIds()
+	{
+		
+		/** remove empty **/
+		$arrSku=array();
+		$arr = explode(",",$this->products);
+		foreach($arr as $sku) {
+			if($sku == '') {
+				continue;
+			}
+			
+			$arrSku[] = $sku;
+		}
+		
+		$c = new CDbCriteria;
+		$c->addInCondition('sku',$arrSku);
+		
+		$products = ProductVariants::model()->findAll($c);
+		if(!$products) {
+			return;
+		}
+		$arrIds=array();
+		foreach($products as $objProduct) {
+			$arrIds[] = $objProduct->id;
+		}
+		$this->products = implode(",",$arrIds);
+		return;		
+	}	
 	
 	public static function model($className=__CLASS__)
 	{
